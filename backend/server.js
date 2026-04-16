3eaaa55d3e4dbd2bb3a0d7a0dfcc4eba28c36350
AzarShahulHameed <azar.irving004@gmail.com>
2026-04-16 22:27:30 +0400

    Remove docker/nginx configs to fix Render routing

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ const cron = require('node-cron');
 const app = express();
 
 // --- middleware ---
+app.use((req, res, next) => {
+  console.log('👉 EXPRESS RECEIVED:', req.method, req.url);
+  next();
+});
+
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
